73eaf4d7ba7ff04ac7a885976df4adb74f9ccc17
Gabriel Ferreira <willamigabrielf.souza@gmail.com>
2023-05-29 09:04:13 -0300

    update 02

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -3,6 +3,11 @@ import './styles/perfil.css'
 const Perfil = () => {
     return (
         <div className='perfil_bg'>
+            <div id="user_info">
+                <img id="user_img" src="https://placehold.co/70x70" alt="user perfil image" />
+                <p>Report for</p>
+                <h2>Jeremy Robson</h2>
+            </div>
             
         </div>
     );
